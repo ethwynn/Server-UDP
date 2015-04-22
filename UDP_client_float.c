ab13053bfce10c58add09948c81d0d43795520d1
ethwynn <fla52310@hotmail.fr>
2015-04-22 11:26:32 +0200

    Adding server which work with float

---
+++ UDP_client_float.c
@@ -0,0 +1,60 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <string.h>
+
+void messageUDP(char *hote,char *service,float message,int taille){
+
+	struct addrinfo precisions,*resultat;
+	int statut;
+	int s;
+
+	/* Creation de l'adresse de socket */
+	memset(&precisions,0,sizeof precisions);
+	precisions.ai_family=AF_UNSPEC;
+	precisions.ai_socktype=SOCK_DGRAM;
+	statut=getaddrinfo(hote,service,&precisions,&resultat);
+	if(statut<0){ perror("messageUDPgenerique.getaddrinfo"); exit(EXIT_FAILURE); }
+
+	/* Creation d'une socket */
+	s=socket(resultat->ai_family,resultat->ai_socktype,resultat->ai_protocol);
+	if(s<0){ perror("messageUDPgenerique.socket"); exit(EXIT_FAILURE); }
+
+	/* Option sur la socket */
+	int vrai=1;
+	if(setsockopt(s,SOL_SOCKET,SO_BROADCAST,&vrai,sizeof(vrai))<0){
+		perror("initialisationServeurUDPgenerique.setsockopt (BROADCAST)");
+		exit(-1);
+	}
+
+	/* Envoi du message */
+	int nboctets=sendto(s,&message,taille,0,resultat->ai_addr,resultat->ai_addrlen);
+	if(nboctets<0){ perror("messageUDPgenerique.sento"); exit(EXIT_FAILURE); }
+
+	/* Liberation de la structure d'informations */
+	freeaddrinfo(resultat);
+
+	/* Fermeture de la socket d'envoi */
+	close(s);
+}
+
+int main(int argc,char *argv[]){
+
+	if(argc!=2){
+		fprintf(stderr,"Syntaxe : %s <serveur>\n",argv[0]);
+		exit(EXIT_FAILURE);
+	}
+	char *hote=argv[1];
+	float message;
+	char *service="5000";
+	float x=10.135432;
+	message=x;
+
+	messageUDP(hote,service,(float)message,sizeof(message));
+	return 0;
+
+}
+
